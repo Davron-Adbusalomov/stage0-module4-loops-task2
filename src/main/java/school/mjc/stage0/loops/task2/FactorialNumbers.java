a8eac553f634a6d50c7f4414123dae7c5f8bff06
Davron-Adbusalomov <115224119+Davron-Adbusalomov@users.noreply.github.com>
2024-02-05 16:58:55 +0500

    Update FactorialNumbers.java

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,30 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
+        int currentNumber = 0;
+
+        while (currentNumber <= printToInclusive) {
+            long factorial = calculateFactorial(currentNumber);
+            System.out.println("Factorial of " + currentNumber + ": " + factorial);
+            currentNumber++;
+        }
+    }
+
+    private long calculateFactorial(int num) {
+        if (num == 0 || num == 1) {
+            return 1;
+        }
+
+        long result = 1;
+        int i = 2;
+
+        while (i <= num) {
+            result *= i;
+            i++;
+        }
+
+        return result;
+    }
 
     }
 }
